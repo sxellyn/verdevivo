1b1257e21c68146666440f3c7bb88cfc3d466e13
sxellyn <suellyngomes@live.com>
2025-05-30 17:40:26 -0300

    [30/05]

---
+++ src/test/java/com/verdevivo/VerdevivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verdevivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdevivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
